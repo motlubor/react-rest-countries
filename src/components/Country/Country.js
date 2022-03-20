abf657b3edb73ef3ee6807dd0e3dfb8cfb54b0b1
motlubor <motlubor2591997@gmail.com>
2022-03-20 17:35:16 +0600

    first commit

---
+++ src/components/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Country.css';
+
+const Country = (props) => {
+    const { area, region, population, name, flags } = props.country;
+    return (
+        <div className='country'>
+            <h2>Country Name:{name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p>Region: {region}</p>
+        </div>
+    );
+};
+
+export default Country;
